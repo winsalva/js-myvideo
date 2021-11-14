0218c3e6d103cb2acdcf5e2183c6b099179b9431
* <winsalva396@gmail.com>
2021-11-14 10:23:28 +0800

    Fetch api test

---
+++ js/app.js
@@ -0,0 +1,29 @@
+async function getUploads() {
+    let url = 'https://myvideo.gigalixirapp.com/api/uploads';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderUploads() {
+    let uploads = await getUploads();
+    let data = uploads.data;
+    let html = '';
+    
+    data.forEach(upload => {
+        let htmlSegment =
+	    `<div class="uploads">
+<h2>${upload.title}>
+                        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('#root');
+    container.innerHTML = html;
+}
+
+renderUploads();
